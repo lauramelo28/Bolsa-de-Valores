5316210f601ca2d82ed07da5d74b1d9344b01dbc
Laura Melo <lauraenisiam@gmail.com>
2024-04-10 19:11:36 -0300

    instalação rabbitMQ

---
+++ java-rabbitMQ/src/Send.java
@@ -0,0 +1,26 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("gull.rmq.cloudamqp.com"); //cluster é o endereço do servidor
+        factory.setUsername("gugkzyzc");
+        factory.setPassword("g_2i_cDGP5kzu-fFabbs7QPIpmI4uQXF");
+        factory.setVirtualHost("gugkzyzc");
+
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
